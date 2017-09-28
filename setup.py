2abcd307a65f1c44991290e155a66314ac09012e
Yu Kumagai <yuuukuma@gmail.com>
2017-09-28 16:08:46 +0900

    Ran the setuptools.

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PyDefect',
+    version='0.1.dev0',
+    author='Yu Kumagai',
+    author_email='yuuukuma@gmail.com',
+    url='https://scholar.google.co.jp/citations?user=xST4MSEAAAAJ&hl=ja',
+    packages=['bin','test',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+    ],
+)
