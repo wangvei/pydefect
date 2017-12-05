c6b4af81823ce8ae847612b2570fc8680308fba2
Yu Kumagai <yuuukuma@gmail.com>
2017-12-05 16:36:37 +0900

    Still temporary for defect_input.

--- input_generator/defect_input.py
+++ input_generator/defect_input.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
 import numpy as np
+import warnings
 import argparse 
 from pymatgen.core.structure import Structure
 from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
-#from pymatgen.core.periodic_table import Element
+from pymatgen.core.periodic_table import Element
 #from pymatgen.core.periodic_table import Element, Specie, get_el_sp
 
 #import parse_poscar as ppos
@@ -23,11 +24,10 @@ __date__ = "December 4, 2017"
 
 class DefectIn():
 
-    def __init__(self, structure, dopants=False, interstitials=False, 
-                 antisites=False, ElNeg_diff=1.0, symbreak=True, symprec=1e-5):
+    def __init__(self, structure, charges=False, dopants=False, interstitials=False, 
+                 antisites=False, ElNeg_diff=1.0, irreguar=None, symbreak=True, symprec=1e-5):
 
         # only vasp5 POSCAR format is supported.
-#        self.structure = structure
         self.dopants = dopants
         self.interstitials = interstitials
         self.antisites = antisites
@@ -38,64 +38,52 @@ class DefectIn():
         symm_finder = SpacegroupAnalyzer(structure)
         self.symm_structure = symm_finder.get_symmetrized_structure()
         self.equiv_site_seq = self.symm_structure.equivalent_sites
-
+        
         elements = []
         frac_coords = []
+        # repr_frac_coords e.g.,[["Mg1", "Mg", 2, [0, 0, 0]],
+        #                        ["Mg2", "Mg", 4, [0.5, 0.5, 0.5]]]
+        repr_frac_coords = []
+        are_elements = {}
+        electron_negativity = {}
+        oxidation_states = {}
 
+        for s in structure.symbol_set: 
+            print(s)
+            are_elements[s] = 0
+        
+            try: 
+                electron_negativity[s] = float(atom.electron_negativity[s])
+            except:
+                warnings.warn("The electron negativity of", s, "is unavailable.")
+                electron_negativity[s] = "N.A."
+
+            try: 
+                oxidation_states[s] = Element(s).common_oxidation_states[-1]
+            except:
+                warnings.warn("The oxidation state of", s, "is unavailable.")
+                oxidation_states[s] = "N.A."
+
+        self.electron_negativity = electron_negativity
+        self.oxidation_states = oxidation_states
+                
         for inequiv_site in self.equiv_site_seq:
-            
+            element = inequiv_site[0].species_string
+            are_elements[element] += 1 # increment nr of inequiv site
+            nr_sites = len(inequiv_site)
+            coords = inequiv_site[0].frac_coords
+            repr_frac_coords.append([element+str(are_elements[element]), 
+                                     element, nr_sites, coords])
             for equiv_site in inequiv_site:
                 elements.append(equiv_site.species_string)
                 frac_coords.append(equiv_site.frac_coords)
 
-        # atoms are sorted by symmetry
+        # Atoms are sorted by symmetry.
         self.structure = Structure(structure.lattice, elements, frac_coords)
+        self.repr_frac_coords = repr_frac_coords
 
-        # Obtain electron negativity and formal charges.
-        electron_negativity = {}
-        charge = {}
-        
-        for i in name:
-            try: 
-                electron_negativity[name[i][0:-1]] = \
-                                 float(atom.electron_negativity[name[i][0:-1]])
-            except:
-                print "The electron negativity of", name[i][0:-1], "cannot be obtained."
-                electron_negativity[name[i][0:-1]] = "N.A."
-        
-            try: 
-                charge[name[i][0:-1]] = int(atom.formal_charges[name[i][0:-1]])
-            except:
-                print "The charge of", name[i][0:-1], "cannot be obtained."
-                charge[name[i][0:-1]] = "N.A."
-        
-        # Note that "lattice_vectors" is a list so that the index should be reduced by 1.
-        for i in sorted(atom_map):
-            print " Name:", name[i]
-            print "  Rep:", i
-            print " Equi:", _pretty_printing_numbers(atom_map[i])
-            print "Coord: %7.5f %7.5f %7.5f" % tuple(atomic_pos[i-1])
-            print "ElNeg:", electron_negativity[name[i][0:-1]]
-            print "Charg:",
-            if charge[name[i][0:-1]] > 0:
-                print ' '.join(['%-2s' % (str(i),) 
-                                          for i in range(0, charge[name[i][0:-1]] + 1)])
-            else:
-                print ' '.join(['%-2s' % (str(i),) 
-                                          for i in range(charge[name[i][0:-1]], 1)])
-            print ""
-        
-        if opts.interstitials:
-            if not len(opts.interstitials) % 3 == 0:
-                print "The interstitial coordinates are not proper. Bye."
-                sys.exit(1)
-            print " Int_site:", tuple([opts.interstitials[i:i + 3]
-                                         for i in range(0, len(opts.interstitials), 3)])
-        
-        if opts.antisite:
-            print " Antisite:",
-        
-            antisite = []
+        if antisites = True:
+            x = []
             for i in list(it.combinations(name.keys(), 2)):
                 if name[i[0]][0:-1] == name[i[1]][0:-1]: continue
             
@@ -107,56 +95,98 @@ class DefectIn():
         
                     if not name1 in antisite: antisite.append(name1)
                     if not name2 in antisite: antisite.append(name2)
+
+        # Obtain electron negativity and formal charges.
         
-            print " ".join(antisite)
         
-        if opts.dopants:
-            for i, elem in enumerate(opts.dopants):
-                try: 
-                    electron_negativity[elem] = atom.electron_negativity[elem]
-                except:
-                    print "The electron negativity of", elem, "cannot be obtained."
-                    electron_negativity[elem] = "N.A."
-            
-                try: 
-                    charge[elem] = atom.formal_charges[elem]
-                except:
-                    print "The electron negativity of", elem, "cannot be obtained."
-                    charge[elem] = "N.A."
-            
-            for i in range(len(opts.dopants)):
-                print " Name:", opts.dopants[i]
-                print "ElNeg:", electron_negativity[opts.dopants[i]]
-                print "Charg:",
-                if charge[opts.dopants[i]] > 0:
-                    print ' '.join(['%-2s' % (str(i),) 
-                                        for i in range(0, charge[opts.dopants[i]] + 1)])
-                else:
-                    print ' '.join(['%-2s' % (str(i),) 
-                                        for i in range(charge[opts.dopants[i]],1)])
-                print ""
-        
-            print "Dopant_site:",
-            for i in opts.dopants:
-                for j, k in name.iteritems():
-                    ENdiff = abs(electron_negativity[i] - electron_negativity[k[0:-1]])
-                    if ENdiff < opts.endiff:
-                        print i + "_" + k,
-            print ""
-        
-        print "Sym_break:", opts.symbreak
-        print "Irregular:"
+#        # Note that "lattice_vectors" is a list so that the index should be reduced by 1.
+#        for i in sorted(atom_map):
+#            print " Name:", name[i]
+#            print "  Rep:", i
+#            print " Equi:", _pretty_printing_numbers(atom_map[i])
+#            print "Coord: %7.5f %7.5f %7.5f" % tuple(atomic_pos[i-1])
+#            print "ElNeg:", electron_negativity[name[i][0:-1]]
+#            print "Charg:",
+#            if charge[name[i][0:-1]] > 0:
+#                print ' '.join(['%-2s' % (str(i),) 
+#                                          for i in range(0, charge[name[i][0:-1]] + 1)])
+#            else:
+#                print ' '.join(['%-2s' % (str(i),) 
+#                                          for i in range(charge[name[i][0:-1]], 1)])
+#            print ""
+#        
+#        if opts.interstitials:
+#            if not len(opts.interstitials) % 3 == 0:
+#                print "The interstitial coordinates are not proper. Bye."
+#                sys.exit(1)
+#            print " Int_site:", tuple([opts.interstitials[i:i + 3]
+#                                         for i in range(0, len(opts.interstitials), 3)])
+#        
+#        if opts.antisite:
+#            print " Antisite:",
+#        
+#            antisite = []
+#            for i in list(it.combinations(name.keys(), 2)):
+#                if name[i[0]][0:-1] == name[i[1]][0:-1]: continue
+#            
+#                ENdiff = abs(electron_negativity[name[i[0]][0:-1]] - \
+#                             electron_negativity[name[i[1]][0:-1]])
+#                if ENdiff < opts.antisite:
+#                    name1 = name[i[0]][0:-1] + "_" + name[i[1]]
+#                    name2 = name[i[1]][0:-1] + "_" + name[i[0]]
+#        
+#                    if not name1 in antisite: antisite.append(name1)
+#                    if not name2 in antisite: antisite.append(name2)
+#        
+#            print " ".join(antisite)
+#        
+#        if opts.dopants:
+#            for i, elem in enumerate(opts.dopants):
+#                try: 
+#                    electron_negativity[elem] = atom.electron_negativity[elem]
+#                except:
+#                    print "The electron negativity of", elem, "cannot be obtained."
+#                    electron_negativity[elem] = "N.A."
+#            
+#                try: 
+#                    charge[elem] = atom.formal_charges[elem]
+#                except:
+#                    print "The electron negativity of", elem, "cannot be obtained."
+#                    charge[elem] = "N.A."
+#            
+#            for i in range(len(opts.dopants)):
+#                print " Name:", opts.dopants[i]
+#                print "ElNeg:", electron_negativity[opts.dopants[i]]
+#                print "Charg:",
+#                if charge[opts.dopants[i]] > 0:
+#                    print ' '.join(['%-2s' % (str(i),) 
+#                                        for i in range(0, charge[opts.dopants[i]] + 1)])
+#                else:
+#                    print ' '.join(['%-2s' % (str(i),) 
+#                                        for i in range(charge[opts.dopants[i]],1)])
+#                print ""
+#        
+#            print "Dopant_site:",
+#            for i in opts.dopants:
+#                for j, k in name.iteritems():
+#                    ENdiff = abs(electron_negativity[i] - electron_negativity[k[0:-1]])
+#                    if ENdiff < opts.endiff:
+#                        print i + "_" + k,
+#            print ""
+#        
+#        print "Sym_break:", opts.symbreak
+#        print "Irregular:"
 
 
     @classmethod
     def from_file(cls, poscar="POSCAR",dopants=False, interstitials=False, 
-                  antisites=False, xi_diff=1.0, symbreak=True, symprec=1e-5):
+                  antisites=False, ElNeg_diff=1.0, symbreak=True, symprec=1e-5):
         """
         Construct DefectIn class object from a POSCAR file.
         """
         structure = Structure.from_file(poscar)
         return cls(structure, dopants=False, interstitials=False, 
-                   antisites=False, xi_diff=1.0, symbreak=True, symprec=1e-5)
+                   antisites=False, ElNeg_diff=1.0, symbreak=True, symprec=1e-5)
 
 
     @classmethod
@@ -174,7 +204,7 @@ class DefectIn():
         self.structure.to(fmt="poscar",filename=filename2)
                 
     def _pretty_printing_defect_in(self,filename="defect.in"):
-
+        pass
 
 
 
