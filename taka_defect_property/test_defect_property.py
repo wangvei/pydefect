b8d3e83ac60824dd00849bc763807b667ae103cd
takahashi_akira_36m <takahashi.akira.36m@gmail.com>
2017-12-07 19:24:26 +0900

    Merge taka_defect_property branch into master

--- taka_defect_property/test_defect_property.py
+++ taka_defect_property/test_defect_property.py
@@ -5,13 +5,13 @@ from defect_property import DefectProperty
 
 dir_name = sys.argv[1]
 dp_from_dir = DefectProperty.from_directory(dir_name)
-dp_from_dir.to(filename="dp_from_dir.dat")
-dp_file = DefectProperty.from_file("dp_from_dir.dat")
-dp_file.to(filename="dp_from_file.dat")
+dp_from_dir.to(filename="dp_from_dir.json")
+dp_file = DefectProperty.from_file("dp_from_dir.json")
+dp_file.to(filename="dp_from_file.json")
 d = dp_from_dir.as_dict()
 dp_dict = DefectProperty.from_dict(d)
-dp_dict.to(filename="dp_from_dict.dat")
+dp_dict.to(filename="dp_from_dict.json")
 s = dp_from_dir.to()
 dp_str = DefectProperty.from_str(s)
-dp_str.to(filename="dp_from_str.dat")
+dp_str.to(filename="dp_from_str.json")
 
