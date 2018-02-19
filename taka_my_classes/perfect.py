7cb489124d23a480b706106050711da7fe913390
takahashi_akira_36m <takahashi.akira.36m@gmail.com>
2018-02-19 11:45:03 +0900

    Add information like author, email to perfect.py

--- taka_my_classes/perfect.py
+++ taka_my_classes/perfect.py
@@ -1,6 +1,13 @@
-# TODO: Make unittest
 from pymatgen.io.vasp.inputs import Poscar
 from pymatgen.io.vasp.outputs import Outcar, Vasprun
+
+__author__ = "Akira Takahashi"
+__copyright__ = "Copyright 2017, Oba group"
+__version__ = "0.1"
+__maintainer__ = "Akira Takahashi"
+__email__ = "takahashi.akira.36m@gmail.com"
+__status__ = "Development"
+__date__ = "February 19, 2018"
 
 
 class Perfect:
