c403b41656ec7193a25d7d0d90d4cc945fe1972c
takahashi_akira_36m <takahashi.akira.36m@gmail.com>
2018-02-22 10:26:19 +0900

    Implement energy_correction
    Commit to update the version.
    Energy correction still does not work.

--- taka_my_classes/energy_correction.py
+++ taka_my_classes/energy_correction.py
@@ -7,10 +7,10 @@ import numpy as np
 import scipy
 import scipy.constants as sconst
 from functools import reduce
-from pymatgen.core.structure import Structure
-from pymatgen.core.periodic_table import Element, get_el_sp
 from pymatgen.io.vasp.inputs import Poscar
-from enum import Enum
+from pymatgen.core.structure import Structure
+from pydefect.taka_my_classes.perfect import Unitcell, Supercell
+from pydefect.input_generator.input_maker import Defect
 
 __author__ = "Akira Takahashi"
 __copyright__ = "Copyright 2017, Oba group"
@@ -19,6 +19,42 @@ __maintainer__ = "Akira Takahashi"
 __email__ = "takahashi.akira.36m@gmail.com"
 __status__ = "Development"
 __date__ = "December 8, 2017"
+
+
+def correct_energy(perfect_unitcell,
+                   perfect_supercell,
+                   defect_input,
+                   defect_raw_energy,
+                   defect_electrostatic_potential,
+                   defect_structure,
+                   method = "ExtendedFNV"):
+    """
+    Args:
+        perfect_unitcell (Unitcell):
+        perfect_supercell (Supercell)
+        defect_input (Defect):
+        defect_raw_energy (float):
+        defect_electrostatic_potential (list of float):
+        defect_structure (Structure):
+        method (str): method to correct energy.
+                      Now "no_correction" and "extended_FNV" is available.
+
+    Returns (float): Corrected energy.
+    """
+    implemented_method = ["extended_FNV", "no_correction"]
+    if method not in implemented_method:
+        raise ValueError("{} method is not implemented.".format(method))
+    alignment = "Not defined"
+    if method == "no_correction":
+        alignment = 0
+    elif method == "extended_FNV":
+        alignment = \
+            _compute_alignment_by_extended_fnv(perfect_unitcell,
+                                               perfect_supercell,
+                                               defect_input,
+                                               defect_electrostatic_potential,
+                                               defect_structure)
+    return defect_raw_energy + alignment
 
 
 def calc_ewald_real_pot(ewald, dielectric_tensor,
@@ -79,7 +115,7 @@ def calc_model_pot_and_lat_energy(ewald,
         = [np.dot(axis, v - defect_pos) for v in atomic_pos_wo_defect]
     coeff = charge * sconst.elementary_charge * \
         1.0e10 / sconst.epsilon_0  # [V]
-    model_pot = [None for i in atomic_pos_wo_defect]
+    model_pot = [None for _ in atomic_pos_wo_defect]
     for i, r in enumerate(atomic_pos_wo_defect):
         set_r_vectors = set_R_vectors - atomic_pos_wrt_defect[i]
         real_part\
@@ -113,12 +149,12 @@ def calc_model_pot_and_lat_energy(ewald,
 
 def get_distance_two_planes(lattice_vectors):
     # (a_i \times a_j) \ddot a_k / |a_i \times  a_j| 
-    distance = np.zeros(3, dtype=float)
+    distance = np.zeros(3, dtype = float)
     for i in range(3):
-        a_i_times_a_j = np.cross(lattice_vectors[i-2], lattice_vectors[i-1])
+        a_i_times_a_j = np.cross(lattice_vectors[i - 2], lattice_vectors[i - 1])
         a_k = lattice_vectors[i]
-        distance[i] = abs(np.dot(a_i_times_a_j, a_k)) \
-                         / np.linalg.norm(a_i_times_a_j)
+        distance[i] = abs(np.dot(a_i_times_a_j, a_k)) / \
+            np.linalg.norm(a_i_times_a_j)
     return distance
 
 
@@ -139,66 +175,73 @@ def calc_average_potential_difference(distance,
     return np.mean(pot_diff)
 
 
-class _DefType(Enum):
-    VACANCY = 1
-    SUBSTITUTIONAL = 2
-    INTERSTITIAL = 3
+def _compute_alignment_by_extended_fnv(perfect_unitcell,
+                                       perfect_supercell,
+                                       defect_input,
+                                       defect_electrostatic_potential,
+                                       defect_structure):
 
+    """
+    Corrects error of formation energy of point defect due to finite-supercell
+    effect.
+    Args:
+        perfect_unitcell (Unitcell):
+        perfect_supercell (Supercell):
+        defect_input (Defect):
+        defect_electrostatic_potential (list of float):
+        defect_structure (Structure):
 
-def correct_energy(defect_dict, correct_dict):
+    Returns (float): Corrected energy by extended FNV method.
 
-    axis = defect_dict["axis"]
-    elements = [ Element(e_name) for e_name in defect_dict["elements"] ]
-    def_coord_frac = defect_dict["frac_coords"]
-    def_structure = Structure(axis, elements, def_coord_frac)
-    volume = def_structure.lattice.volume
+    """
+    axis = np.array(perfect_unitcell.structure.lattice.matrix)
+    defect_coords = [site.coords for site in defect_structure.sites]
+    volume = defect_structure.lattice.volume
 
     # read defect_pos (fractional coordination)
+    # TODO: implement after complete Defect class
     read_dpos = defect_dict["defect_position"]
     if isinstance(read_dpos, int):
-        defect_pos = np.array(def_coord_frac[read_dpos - 1])
+        defect_pos = np.array(defect_coords[read_dpos - 1])
     elif isinstance(read_dpos, list):
         defect_pos = np.array(read_dpos)
     else:
         raise TypeError("Could not read defect_pos.")
-
     ref_pot = np.array(correct_dict["reference_potential"])
-    def_pot = np.array(defect_dict["atomic_site_pot"])
-    if len(def_pot) == len(ref_pot):
-        def_type = _DefType.SUBSTITUTIONAL
+    if len(defect_electrostatic_potential) == len(ref_pot):
         defect_index = read_dpos
-        atomic_pos_wo_defect = np.delete(def_coord_frac, defect_index-1, 0)
-    elif len(def_pot) == len(ref_pot)+1:
-        def_type = _DefType.INTERSTITIAL
+        atomic_position_without_defect =\
+            np.delete(defect_coords, defect_index - 1, 0)
+    elif len(defect_electrostatic_potential) == len(ref_pot) + 1:
         defect_index = read_dpos
-        def_pot = np.delete(def_pot, defect_index-1)
-        atomic_pos_wo_defect = np.delete(def_coord_frac, defect_index-1, 0)
-    elif len(def_pot) == len(ref_pot)-1:
-        def_type = _DefType.VACANCY
-        defect_index = 192 # in outcar, index 1~, temporary hard coding
-        ref_pot = np.delete(ref_pot, defect_index-1)
+        defect_electrostatic_potential =\
+            np.delete(defect_electrostatic_potential, defect_index - 1)
+        atomic_position_without_defect =\
+            np.delete(defect_coords, defect_index - 1, 0)
+    elif len(defect_electrostatic_potential) == len(ref_pot) - 1:
+        defect_index = 192  # in outcar, index 1~, temporary hard coding
+        ref_pot = np.delete(ref_pot, defect_index - 1)
         print("warning: defect index should be automatic implement")
-        atomic_pos_wo_defect = def_coord_frac
+        atomic_position_without_defect = defect_coords
     else:
         raise ValueError("This code can not applied to more than one defect.")
 
-    diff_pot = -(def_pot - ref_pot)
+    diff_pot = -(defect_electrostatic_potential - ref_pot)
 
     distances_from_defect = np.array(defect_dict["distance_from_defect"])
 
-    dielectric_tensor = np.array(correct_dict["dielectric_tensor"])\
-                      + np.array(correct_dict["dielectric_ionic_tensor"])
+    dielectric_tensor = perfect_unitcell.dielectric_tensor
 
-    charge = defect_dict["charge"]
-
-    ewald = correct_json["ewald"]
+    charge = defect_input.charge
+    # TODO: check ewald or ewald_param?
+    ewald = perfect_unitcell.get_ewald_param()
     set_R_vectors = np.array(correct_json["set_R_vector"])
     set_G_vectors = np.array(correct_json["set_G_vector"])
     # potential.sh 3
     model_pot, model_pot_site, lattice_energy \
         = calc_model_pot_and_lat_energy(ewald,
                                         charge,
-                                        atomic_pos_wo_defect,
+                                        atomic_position_without_defect,
                                         defect_pos,
                                         dielectric_tensor,
                                         volume,
@@ -215,4 +258,5 @@ def correct_energy(defect_dict, correct_dict):
     print("potential difference = {0}".format(ave_pot_diff))
     alignment = -1.0 * ave_pot_diff * charge
     print("alignment-like term = {0}".format(alignment))
+    return alignment
 
