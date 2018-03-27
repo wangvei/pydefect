474293850683df218b4d9d77e27d59414c9c20ff
kuma <yuuukuma@gmail.com>
2018-03-27 15:23:33 +0900

    Delete recommend_supercell_ase_cythonized

--- input_maker/recommend_supercell_ase_cythonized/setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Build import cythonize
-import numpy
-
-setup(name="ase_cython",
-      ext_modules=cythonize("ase_cython.pyx"),
-      include_dirs = [numpy.get_include()])
