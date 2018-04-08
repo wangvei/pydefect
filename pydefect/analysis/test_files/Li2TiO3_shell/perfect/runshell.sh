4b4e073d23beb366e76783f711654dca43af82bf
kuma <yuuukuma@gmail.com>
2018-04-08 14:08:23 +0900

    Change directory structure for input_maker

--- pydefect/analysis/test_files/Li2TiO3_shell/perfect/runshell.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-#============ LSF Options =============
-#QSUB -q gr10261b 
-#QSUB -W 336:00 
-#QSUB -A p=16:t=1:c=1:m=3840M 
-#QSUB -rn
-#============ Shell Script ============
-
-zsh ~/script/shell/repeatGeomOpt5.sh mpiexec.hydra  /home/b/b32282/bin/vasp/vasp533GGA
