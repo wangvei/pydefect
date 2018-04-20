307692364f8e296bb68d4c62d859c81537b45b1e
takahashi_akira_36m <takahashi.akira.36m@gmail.com>
2018-04-20 10:53:03 +0900

    Add substitutional test (the test is passed)

--- pydefect/core/tests/test_correction.py
+++ pydefect/core/tests/test_correction.py
@@ -36,21 +36,23 @@ expected_vacancy_potential_difference = 0.2812066
 expected_vacancy_alignment_like_term = -0.5624132
 expected_vacancy_lattice_energy = -1.2670479
 expected_vacancy_symbols = ["Mg"] * 8 + ["O"] * 7
-expected_vacancy_model_pot = [-0.221616953329,
-                              -0.0757569991956,
-                              -0.0745712827233,
-                              -0.0770321065632,
-                              -0.0755424109893,
-                              -0.0784910684584,
-                              -0.0792252195274,
-                              -0.22161641786,
-                              -0.160982214428,
-                              -0.160966818748,
-                              -0.16098264708,
-                              -0.160975656079,
-                              -0.160984578901,
-                              -0.160983417362,
-                              -0.30115082168]
+expected_vacancy_model_pot = [
+    -0.221616953329,
+    -0.0757569991956,
+    -0.0745712827233,
+    -0.0770321065632,
+    -0.0755424109893,
+    -0.0784910684584,
+    -0.0792252195274,
+    -0.22161641786,
+    -0.160982214428,
+    -0.160966818748,
+    -0.16098264708,
+    -0.160975656079,
+    -0.160984578901,
+    -0.160983417362,
+    -0.30115082168
+]
 expected_vacancy_difference_electrostatic_pot = [
     -0.7019,
     0.2297,
@@ -68,21 +70,23 @@ expected_vacancy_difference_electrostatic_pot = [
     0.2584,
     0.6767
 ]
-expected_vacancy_distances_list = [3.67147,
-                                   2.25636,
-                                   2.25288,
-                                   2.26013,
-                                   2.25573,
-                                   2.26446,
-                                   2.26664,
-                                   3.67347,
-                                   2.99998,
-                                   2.99316,
-                                   2.99870,
-                                   2.99665,
-                                   3.00100,
-                                   2.99985,
-                                   4.24097]
+expected_vacancy_distances_list = [
+    3.67147,
+    2.25636,
+    2.25288,
+    2.26013,
+    2.25573,
+    2.26446,
+    2.26664,
+    3.67347,
+    2.99998,
+    2.99316,
+    2.99870,
+    2.99665,
+    3.00100,
+    2.99985,
+    4.24097
+]
 
 # interstitial
 dirname_interstitial = test_dir + "/defects/Mg_i1_2/"
@@ -147,6 +151,66 @@ expected_interstitial_distances_list = [
     3.52014,
 ]
 
+# substitutional
+dirname_substitutional = test_dir + "/defects/Al_Mg1_1/"
+sub_defect_entry_json = dirname_substitutional + "/defect_entry.json"
+# calculated by shell script made by Dr. Kumagai
+expected_substitutional_potential_difference = -0.493117
+expected_substitutional_alignment_like_term = 0.4931176
+expected_substitutional_lattice_energy = -0.3167620
+expected_substitutional_symbols = ["Mg"] * 7 + ["O"] * 8
+expected_substitutional_model_pot = [
+    -0.0804919304029,
+    -0.0804916055806,
+    -0.0804914783527,
+    -0.0804906118397,
+    -0.0804906768674,
+    -0.0804917174989,
+    -0.150575724122,
+    -0.11080882505,
+    0.00892143179861,
+    0.00853956104979,
+    0.00811480817918,
+    0.00754938028252,
+    0.0078962308938,
+    0.00501553618882,
+    -0.110808711372,
+]
+expected_substitutional_difference_electrostatic_pot = [
+    -0.7186,
+    -0.7260,
+    -0.7281,
+    -0.7248,
+    -0.7282,
+    -0.7314,
+    0.5708,
+    -0.7538,
+    -0.0456,
+    -0.0480,
+    -0.0518,
+    -0.0554,
+    -0.0572,
+    -0.0661,
+    -0.7531,
+]
+expected_substitutional_distances_list = [
+    3.00289,
+    3.00095,
+    3.00073,
+    2.99927,
+    2.99977,
+    3.00157,
+    4.24062,
+    3.67428,
+    2.02070,
+    2.02236,
+    2.02421,
+    2.02668,
+    2.02516,
+    2.03783,
+    3.67394,
+]
+
 
 class EwaldTest(unittest.TestCase):
 
@@ -195,32 +259,34 @@ class CorrectionTest(unittest.TestCase):
         self._vacancy_entry = DefectEntry.json_load(vac_defect_entry_json)
         self._vacancy = SupercellDftResults.from_vasp_files(dirname_vacancy)
         self._interstitial_entry = DefectEntry.json_load(int_defect_entry_json)
-        self._interstitial = SupercellDftResults.from_vasp_files(dirname_interstitial)
-        print("Correction_test setUp completed")
+        self._interstitial = \
+            SupercellDftResults.from_vasp_files(dirname_interstitial)
+        self._substitutional_entry = DefectEntry.json_load(sub_defect_entry_json)
+        self._substitutional = \
+            SupercellDftResults.from_vasp_files(dirname_substitutional)
 
     def test_compute_extended_fnv(self):
         # vacancy
-        # vacancy_correction = \
-        #     Correction.compute_correction(self._vacancy_entry,
-        #                                   self._vacancy,
-        #                                   self._perfect,
-        #                                   self._unitcell)
-        # self.assertAlmostEqual(vacancy_correction.lattice_energy,
-        #                        expected_vacancy_lattice_energy, 4)
-        # self.assertAlmostEqual(vacancy_correction.diff_ave_pot,
-        #                        expected_vacancy_potential_difference, 5)
-        # self.assertAlmostEqual(vacancy_correction.alignment,
-        #                        expected_vacancy_alignment_like_term, 5)
-
+        vacancy_correction = \
+            Correction.compute_correction(self._vacancy_entry,
+                                          self._vacancy,
+                                          self._perfect,
+                                          self._unitcell)
+        self.assertAlmostEqual(vacancy_correction.lattice_energy,
+                               expected_vacancy_lattice_energy, 4)
+        self.assertAlmostEqual(vacancy_correction.diff_ave_pot,
+                               expected_vacancy_potential_difference, 5)
+        self.assertAlmostEqual(vacancy_correction.alignment,
+                               expected_vacancy_alignment_like_term, 5)
         # TODO: Check case of irreducible sites like O1, O2
-        # assert_array_equal(vacancy_correction.symbols_without_defect,
-        #                    expected_vacancy_symbols)
-        # assert_array_almost_equal(vacancy_correction.distances_from_defect,
-        #                           expected_vacancy_distances_list, 5)
-        # assert_array_almost_equal(vacancy_correction.model_pot,
-        #                           expected_vacancy_model_pot, 5)
-        # assert_array_almost_equal(vacancy_correction.difference_electrostatic_pot,
-        #                           expected_vacancy_difference_electrostatic_pot, 5)
+        assert_array_equal(vacancy_correction.symbols_without_defect,
+                           expected_vacancy_symbols)
+        assert_array_almost_equal(vacancy_correction.distances_from_defect,
+                                  expected_vacancy_distances_list, 5)
+        assert_array_almost_equal(vacancy_correction.model_pot,
+                                  expected_vacancy_model_pot, 5)
+        assert_array_almost_equal(vacancy_correction.difference_electrostatic_pot,
+                                  expected_vacancy_difference_electrostatic_pot, 5)
 
         # interstitial
         interstitial_correction = \
@@ -234,7 +300,6 @@ class CorrectionTest(unittest.TestCase):
                                expected_interstitial_potential_difference, 5)
         self.assertAlmostEqual(interstitial_correction.alignment,
                                expected_interstitial_alignment_like_term, 5)
-
         # TODO: Check case of irreducible sites like O1, O2
         assert_array_equal(interstitial_correction.symbols_without_defect,
                            expected_interstitial_symbols)
@@ -242,25 +307,54 @@ class CorrectionTest(unittest.TestCase):
                                   expected_interstitial_distances_list, 5)
         assert_array_almost_equal(interstitial_correction.model_pot,
                                   expected_interstitial_model_pot, 5)
-        assert_array_almost_equal(interstitial_correction.difference_electrostatic_pot,
-                                  expected_interstitial_difference_electrostatic_pot, 5)
+        assert_array_almost_equal(
+            interstitial_correction.difference_electrostatic_pot,
+            expected_interstitial_difference_electrostatic_pot, 5)
+
+        # substitutional
+        substitutional_correction = \
+            Correction.compute_correction(self._substitutional_entry,
+                                          self._substitutional,
+                                          self._perfect,
+                                          self._unitcell)
+        self.assertAlmostEqual(substitutional_correction.lattice_energy,
+                               expected_substitutional_lattice_energy, 4)
+        self.assertAlmostEqual(substitutional_correction.diff_ave_pot,
+                               expected_substitutional_potential_difference, 5)
+        self.assertAlmostEqual(substitutional_correction.alignment,
+                               expected_substitutional_alignment_like_term, 5)
+        # TODO: Check case of irreducible sites like O1, O2
+        # Methods to get defect position slightly differs between that of shell
+        # script and pydefect module. Then, distances_from_defect also differ
+        # between shell and pydefect. However, as tested above, difference of
+        # correction energy is not critical at all.
+        assert_array_equal(substitutional_correction.symbols_without_defect,
+                           expected_substitutional_symbols)
+        assert_array_almost_equal(
+            substitutional_correction.distances_from_defect,
+            expected_substitutional_distances_list, 1)
+        assert_array_almost_equal(substitutional_correction.model_pot,
+                                  expected_substitutional_model_pot, 2)
+        assert_array_almost_equal(
+            substitutional_correction.difference_electrostatic_pot,
+            expected_substitutional_difference_electrostatic_pot, 2)
 
     def test_plot_distance_vs_potential(self):
 
-        # vacancy_correction = \
-        #     Correction(CorrectionMethod.extended_fnv,
-        #                self._ewald,
-        #                expected_vacancy_lattice_energy,
-        #                expected_vacancy_potential_difference,
-        #                expected_vacancy_alignment_like_term,
-        #                expected_vacancy_symbols,
-        #                expected_vacancy_distances_list,
-        #                expected_vacancy_difference_electrostatic_pot,
-        #                expected_vacancy_model_pot)
-        # expected_max_sphere_radius = 2.45194
-        # self.assertAlmostEqual(vacancy_correction.max_sphere_radius,
-        #                        expected_max_sphere_radius, 5)
-        # vacancy_correction.plot_distance_vs_potential()
+        vacancy_correction = \
+            Correction(CorrectionMethod.extended_fnv,
+                       self._ewald,
+                       expected_vacancy_lattice_energy,
+                       expected_vacancy_potential_difference,
+                       expected_vacancy_alignment_like_term,
+                       expected_vacancy_symbols,
+                       expected_vacancy_distances_list,
+                       expected_vacancy_difference_electrostatic_pot,
+                       expected_vacancy_model_pot)
+        expected_max_sphere_radius = 2.45194
+        self.assertAlmostEqual(vacancy_correction.max_sphere_radius,
+                               expected_max_sphere_radius, 5)
+        vacancy_correction.plot_distance_vs_potential()
 
         interstitial_correction = \
             Correction(CorrectionMethod.extended_fnv,
@@ -273,6 +367,18 @@ class CorrectionTest(unittest.TestCase):
                        expected_interstitial_difference_electrostatic_pot,
                        expected_interstitial_model_pot)
         interstitial_correction.plot_distance_vs_potential()
+
+        substitutional_correction = \
+            Correction(CorrectionMethod.extended_fnv,
+                       self._ewald,
+                       expected_substitutional_lattice_energy,
+                       expected_substitutional_potential_difference,
+                       expected_substitutional_alignment_like_term,
+                       expected_substitutional_symbols,
+                       expected_substitutional_distances_list,
+                       expected_substitutional_difference_electrostatic_pot,
+                       expected_substitutional_model_pot)
+        substitutional_correction.plot_distance_vs_potential()
 
 
 if __name__ == "__main__":
