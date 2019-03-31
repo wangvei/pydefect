369212a7b760297462b88e6fd80e3f5ec8e9a7ae
kuma <yuuukuma@gmail.com>
2019-03-31 15:37:30 +0900

    Refactoring irreducible_site

--- pydefect/core/irreducible_site.py
+++ pydefect/core/irreducible_site.py
@@ -1,15 +1,12 @@
 # -*- coding: utf-8 -*-
 
+from monty.json import MSONable
+
 __author__ = "Yu Kumagai"
-__copyright__ = "Copyright 2017, Oba group"
-__version__ = "0.1"
 __maintainer__ = "Yu Kumagai"
-__email__ = "yuuukuma@gmail.com"
-__status__ = "Development"
-__date__ = "December 4, 2017"
 
 
-class IrreducibleSite:
+class IrreducibleSite(MSONable):
     """
     This class object holds properties related to the symmetrically equivalent
     atom set.
@@ -23,88 +20,56 @@ class IrreducibleSite:
         element (str):
             Element name (e.g., Mg)
         first_index (int):
-            First index of irreducible_name.
+            First index of irreducible_name. Note that the index begins from 1.
         last_index (int):
             Last index of irreducible_name.
         representative_coords (list):
             Representative coordinates, namely the position of first_index
         wyckoff (str):
-            A wyckoff letter
+            A wyckoff letter.
         site_symmetry (str):
             Site symmetry.
         coordination_distances (dict):
             Coordination environment. An example is
             {"Mg": [1.92, 1.95, 2.01], "Al": [1.82, 1.95]}
+        magmom (float):
+            Local magnetic moment.
     """
     def __init__(self, irreducible_name, element, first_index, last_index,
                  representative_coords, wyckoff=None, site_symmetry=None,
-                 coordination_distances=None):
-        self._irreducible_name = irreducible_name
-        self._element = element
-        self._first_index = first_index
-        self._last_index = last_index
-        self._repr_coords = representative_coords
-        self._wyckoff = wyckoff
-        self._site_symmetry = site_symmetry
-        self._coordination_distances = coordination_distances
+                 coordination_distances=None, magmom=None):
+        self.irreducible_name = irreducible_name
+        self.element = element
+        self.first_index = first_index
+        self.last_index = last_index
+        self.representative_coords = representative_coords
+        self.wyckoff = wyckoff
+        self.site_symmetry = site_symmetry
+        self.coordination_distances = coordination_distances
+        self.magmom = magmom
 
-    def __eq__(self, other):
-        if other is None or type(self) != type(other):
-            raise TypeError
-        return self.__dict__ == other.__dict__
-
+    # These are necessary when IrreducibleSite is recovered in from_dict
+    # in the DefectInitialSetting.
     @classmethod
     def from_dict(cls, d):
         return cls(d["irreducible_name"], d["element"], d["first_index"],
-                   d["last_index"], d["repr_coords"], d["wyckoff"],
+                   d["last_index"], d["representative_coords"], d["wyckoff"],
                    d["site_symmetry"], d["coordination_distances"])
 
     def as_dict(self):
-        d = {"irreducible_name": self._irreducible_name,
-             "element": self._element,
-             "first_index": self._first_index,
-             "last_index": self._last_index,
-             "repr_coords": self._repr_coords,
-             "wyckoff": self._wyckoff,
-             "site_symmetry": self._site_symmetry,
-             "coordination_distances": self._coordination_distances}
+        d = {"irreducible_name": self.irreducible_name,
+             "element": self.element,
+             "first_index": self.first_index,
+             "last_index": self.last_index,
+             "representative_coords": self.representative_coords,
+             "wyckoff": self.wyckoff,
+             "site_symmetry": self.site_symmetry,
+             "coordination_distances": self.coordination_distances}
         return d
-
-    @property
-    def irreducible_name(self):
-        return self._irreducible_name
-
-    @property
-    def element(self):
-        return self._element
-
-    @property
-    def first_index(self):
-        return self._first_index
-
-    @property
-    def last_index(self):
-        return self._last_index
-
-    @property
-    def repr_coords(self):
-        return self._repr_coords
-
-    @property
-    def wyckoff(self):
-        return self._wyckoff
-
-    @property
-    def site_symmetry(self):
-        return self._site_symmetry
-
-    @property
-    def coordination_distances(self):
-        return self._coordination_distances
 
     @property
     def num_atoms(self):
         """
         Returns the number of atoms.
         """
-        return self._last_index - self._first_index + 1
+        return self.last_index - self.first_index + 1
---
+++ pydefect/core/tests/test_irreducible_site.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+import os
+import unittest
+
+from pydefect.core.irreducible_site import IrreducibleSite
+
+__author__ = "Yu Kumagai"
+__maintainer__ = "Yu Kumagai"
+
+test_dir = os.path.join(os.path.dirname(__file__), "..", "..", "..",
+                        "test_files", "core")
+
+
+class PriorInfoTest(unittest.TestCase):
+
+    def setUp(self):
+        """ """
+        irreducible_name = "O1"
+        element = "O"
+        first_index = 33
+        last_index = 64
+        representative_coords = [0, 0, 0]
+        wyckoff = "m3m"
+        site_symmetry = "Oh"
+        coordination_distance = {"Mg": [2, 2, 2, 2, 2, 2]}
+
+        self.O1_MgO = IrreducibleSite(irreducible_name,
+                                      element,
+                                      first_index,
+                                      last_index,
+                                      representative_coords,
+                                      wyckoff,
+                                      site_symmetry,
+                                      coordination_distance)
+
+    def test_dict(self):
+        """ round trip test of to_dict and from_dict
+        """
+        d = self.O1_MgO.as_dict()
+        prior_info_from_dict = IrreducibleSite.from_dict(d)
+        self.assertTrue(d == prior_info_from_dict.as_dict())
+
+
