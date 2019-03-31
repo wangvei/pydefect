6e07ddafd727ed924f08616c842ebaf65b9ea393
kuma <yuuukuma@gmail.com>
2019-03-31 15:38:33 +0900

    Add config file

---
+++ pydefect/core/config.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "Yu Kumagai"
+__maintainer__ = "Yu Kumagai"
+
+__author__ = 'Yu Kumagai'
+
+
+# Following defaults determine the condition of automatic defect calculations.
+# electronegativity difference for antisites and substitutional impurities
+ELECTRONEGATIVITY_DIFFERENCE = 1.0
+# Maximum displacement distance
+DISPLACEMENT_DISTANCE = 0.2
+# Cutoff radius in which atoms are perturbed.
+CUTOFF_RADIUS = 3.0
+SYMMETRY_TOLERANCE = 0.01
+ANGLE_TOL = 5
+
+KPT_DENSITY = 2
+
