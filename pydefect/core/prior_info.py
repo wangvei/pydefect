6c0d7f90ef088c26f0836f2f6c74f753af97ae2f
kuma <yuuukuma@gmail.com>
2019-05-24 14:36:43 +0900

    Modify PriorInfo

--- pydefect/core/prior_info.py
+++ pydefect/core/prior_info.py
@@ -9,48 +9,62 @@ __maintainer__ = "Yu Kumagai"
 
 
 class PriorInfo(MSONable):
-    """ Prior information used for input of first-principles calculations
-
-    Prior information is used to change the input parameters in first-principles
-    calculations
+    """ Prior information used for controlling parameters in DFT calculations
 
     Args:
-        energy_per_atom (float): Energy per atom calculated in the data_source.
-        band_gap (float): Band gap calculated in the data_source.
-        total_magnetization (float): Total total_magnetization in the data_source.
-        data_source (str): The data source
-        is_molecule (bool): Whether the system is molecule or not.
-        mag_threshold (float): Threshold to judge if the system is magnetic.
-        band_gap_threshold (float): Threshold to judge if the system is metal.
-
+        energy_per_atom (float):
+            Energy per atom calculated in the data_source.
+        band_gap (float):
+            Band gap calculated in the data_source.
+        total_magnetization (float):
+            Total total_magnetization in the data_source.
+        data_source (str):
+            The data source
+        is_molecule (bool):
+            Whether the system is molecule or not.
+        mag_threshold (float):
+            Threshold to judge if the system is magnetic.
+        band_gap_threshold (float):
+            Threshold to judge if the system is metal.
     """
     def __init__(self,
                  energy_per_atom: float = None,
                  band_gap: float = None,
                  total_magnetization: float = None,
                  data_source: str = None,
-                 is_molecule: bool = None,
-                 mag_threshold: float = 0.01,
+                 is_cluster: bool = None,
+                 mag_threshold: float = 0.05,
                  band_gap_threshold: float = 0.1):
         self.energy_per_atom = energy_per_atom
         self.band_gap = band_gap
         self.total_magnetization = total_magnetization
         self.data_source = data_source
-        self.is_molecule = is_molecule
+        self.is_cluster = is_cluster
         self.mag_threshold = mag_threshold
         self.band_gap_threshold = band_gap_threshold
 
-    def dump_json(self, filename="prior_info.json"):
+    def __str__(self):
+        outs = ["energy_per_atom: " + str(self.energy_per_atom),
+                "band gap: " + str(self.band_gap),
+                "total magnetization: " + str(self.total_magnetization),
+                "data source: " + str(self.data_source),
+                "is cluster: " + str(self.is_cluster)]
+        return "\n".join(outs)
+
+    def dump_json(self, filename: str = "prior_info.json"):
         with open(filename, "w") as fw:
             json.dump(self.as_dict(), fw, indent=2)
 
     @classmethod
-    def load_json(cls, filename="prior_info.json"):
+    def load_json(cls, filename: str = "prior_info.json"):
         return cls.from_dict(loadfn(filename))
 
     @property
     def is_magnetic(self):
-        return self.total_magnetization > self.mag_threshold
+        try:
+            return self.total_magnetization > self.mag_threshold
+        except TypeError:
+            return None
 
     @property
     def has_band_gap(self):
